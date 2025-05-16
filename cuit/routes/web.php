409ef08456e5cb8b35ee3ab20f89e1e4054bf044
Karim-Rimuru <mekar.10817@gmail.com>
2025-05-16 10:48:54 +0700

    Done

--- cuit/routes/web.php
+++ cuit/routes/web.php
@@ -3,9 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/login', function () {
-    return view('ini halaman login');
+    return view('login');
+});
+
+Route::get('/login', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('register');
+});
+
+Route::get('/register', function () {
+    return view('login');
 });
